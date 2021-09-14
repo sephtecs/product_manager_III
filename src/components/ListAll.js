478d5ea06eed2bd43055aca9f411c264ef0df143
Joseph <josephtecson18@gmail.com>
2021-09-14 00:10:04 -0700

    add delete functionality

--- src/components/ListAll.js
+++ src/components/ListAll.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Homepage from './HomePage';
+import { Link } from 'react-router-dom';
 
 const ListAll = (props) => {
 
@@ -17,6 +18,17 @@ const ListAll = (props) => {
         })
     }, [<Homepage />]); //dependency array; [Look up on this feature and its complications a little further!!!]
 
+    const deleteEquipment = (deleteId) => {
+        axios.delete('http://localhost:8000/api/equipment/' + deleteId)
+            .then(res => {
+                const delAllEquipment = allEquipment.filter((equip) => {
+                    return equip._id !== deleteId;
+                })
+                setAllEquipment(delAllEquipment);
+            })
+            .catch(err => console.error)
+    }
+
     return (
         <div style={{textAlign: "center"}}>
             <h2>All Products:</h2>
@@ -24,8 +36,10 @@ const ListAll = (props) => {
             {allEquipment.map((equipment, index) => 
                 <ul style={{textAlign: "center"}}>
                     <a href= {`/equipment/${equipment._id}`}>{equipment.title}</a>
+                    <button style={{marginLeft: "8px"}} onClick={(e) =>{deleteEquipment(equipment._id)}}>Delete</button>
                 </ul>
             )}
+
         </div>
     )
 };
